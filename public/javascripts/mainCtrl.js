939ef68a6aae40c797c7f4c2833b3076cbea4a61
timk <timoteokang@gmail.com>
2015-08-03 03:38:41 -0700

    request methods implemented

--- public/javascripts/mainCtrl.js
+++ public/javascripts/mainCtrl.js
@@ -38,7 +38,37 @@ app.controller('homeCtrl', ['$scope', '$http', '$window', '$location', 'authInte
         });
     };
 
-    $scope.logIn = function () {
+    // $scope.signup = function() {
+
+    //     var credentials = {
+    //         first_name: $scope.first_name,
+    //         last_name: $scope.last_name,
+    //         email: $scope.email,
+    //         password: $scope.password,
+    //         gender: $scope.gender
+    //     };
+
+    //     request({
+    //         url: 'http://localhost:9000/api-signup/',
+    //         method: 'POST',
+    //         body: credentials
+    //     }, function (error, response, body) {
+    //         if (error) {
+    //             // Erase the token if the user fails to log in
+    //             delete $window.sessionStorage.token;
+    //             return console.log('Error:', error);
+    //         }
+    //         else {
+    //             $window.sessionStorage.token = body.jwt;
+    //             $scope.user = body.user;
+    //             $('#signupModal').modal('hide');
+    //             console.log(body.jwt);
+    //             console.log(body.user);
+    //         }
+    //     });
+    // };
+
+    $scope.login = function () {
 
         var credentials = {
             email: $scope.user.email,
@@ -59,10 +89,37 @@ app.controller('homeCtrl', ['$scope', '$http', '$window', '$location', 'authInte
         });
     };
 
+    // $scope.login = function() {
+
+    //     var credentials = {
+    //         email: $scope.user.email,
+    //         password: $scope.user.password
+    //     };
+
+    //     request({
+    //         url: 'http://localhost:9000/api-login/',
+    //         method: 'POST',
+    //         body: credentials
+    //     }, function (error, response, body) {
+    //         if (error) {
+    //             // Erase the token if the user fails to log in
+    //             delete $window.sessionStorage.token;
+    //             return console.log('Error:', error);
+    //         }
+    //         else {
+    //             $window.sessionStorage.token = body.jwt;
+    //             $scope.user = body.user;
+    //             $('#loginModal').modal('hide');
+    //             console.log(body.jwt);
+    //             console.log(body.user);
+    //         }
+    //     });
+    // };
+
     $scope.logout = function () {
         delete $window.sessionStorage.token;
         $window.sessionStorage.clear();
-        $scope.goto('home');
+        $scope.goto('/home');
     };
 
     $scope.createGroop = function() {
@@ -81,121 +138,64 @@ app.controller('homeCtrl', ['$scope', '$http', '$window', '$location', 'authInte
             });
     };
 
-    $scope.getAllGroops = function() {
-    	$http.get('http://localhost:9000/api/groups/')
-    		.success(function(data) {
-    			$scope.allGroops = data;
-    		})
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
-    };
+    // $scope.createGroop = function() {
+
+    //     var groop = {
+    //         name: $scope.groop.name,
+    //         min_members: $scope.groop.min,
+    //         max_members: $scope.groop.max,
+    //         location: $scope.groop.location,
+    //         date: $scope.groop.date,
+    //         description: $scope.groop.description
+    //     }
+
+    //     request({
+    //         url: 'http://localhost:9000/api/groups/',
+    //         method: 'POST',
+    //         body: groop
+    //     }, function (error, response, body) {
+    //         if (error) {
+    //             return console.log('Error:', error);
+    //         }
+    //         else {
+    //             $('#createModal').modal('hide');
+    //             document.getElementById('createGroop').reset();
+    //         }
+    //     });
+    // };
 
     $scope.goto = function(path) {
         $location.path(path);
     };
-
-    // $scope.getAllGroops();
 	
 }]);
 
 // Grooper Search Controller =======================================================================================
 
-app.controller('searchCtrl', ['$scope', '$window', '$location', 'authInterceptor', 'groopsFactory', '$stateParams', function($scope, $window, $location, authInterceptor, groopsFactory, $stateParams) {
+app.controller('searchCtrl', ['$scope', '$window', '$location', 'authInterceptor', 'groopsFactory', '$stateParams', '$http', function($scope, $window, $location, authInterceptor, groopsFactory, $stateParams, $http) {
 
-
-    $scope.signUp = function() {
-        var credentials = {
-            first_name: $scope.first_name,
-            last_name: $scope.last_name,
-            email: $scope.email,
-            password: $scope.password,
-            gender: $scope.gender
-        };
-        
-        $http.post('http://localhost:9000/api-signup/', credentials)
-            .success(function (data, status, headers, config) {
-                $window.sessionStorage.token = data.jwt;
-                $scope.user = data.user;
-                $('#signupModal').modal('hide');
-                console.log(data.jwt);
-                console.log(data.user);
+    $scope.getAllGroops = function() {
+        $http.get('http://localhost:9000/api/groups/')
+            .success(function(data) {
+                $scope.allGroops = data;
+                groopsFactory.groops = $scope.allGroops;
             })
-            .error(function (data, status, headers, config) {
-                // Erase the token if the user fails to log in
-                delete $window.sessionStorage.token;
-        });
-    };
-
-    $scope.logIn = function () {
-
-        var credentials = {
-            email: $scope.user.email,
-            password: $scope.user.password
-        };
-
-        $http.post('http://localhost:9000/api-login/', credentials)
-            .success(function (data, status, headers, config) {
-                $window.sessionStorage.token = data.jwt;
-                $scope.user = data.user;
-                $('#loginModal').modal('hide');
-                console.log(data.jwt);
-                console.log(data.user);
-            })
-            .error(function (data, status, headers, config) {
-                // Erase the token if the user fails to log in
-                delete $window.sessionStorage.token;
-        });
-    };
-
-    $scope.logout = function () {
-        delete $window.sessionStorage.token;
-        $window.sessionStorage.clear();
-        $scope.goto('home');
-    };
-
-    $scope.createGroop = function() {
-        var groop = {
-            name: $scope.groop.name,
-            min_members: $scope.groop.min,
-            max_members: $scope.groop.max,
-            location: $scope.groop.location,
-            date: $scope.groop.date,
-            description: $scope.groop.description
-        }
-        $http.post('http://localhost:9000/api/groups/', groop)
-            .success(function() {
-                $('#createModal').modal('hide');
-                document.getElementById("createGroop").reset();
+            .error(function(data) {
+                console.log('Error: ' + data);
             });
     };
 
     // $scope.getAllGroops = function() {
-    //     $http.get('http://localhost:9000/api/groups/')
-    //         .success(function(data) {
-    //             $scope.allGroops = data;
-    //             groopsFactory.groops = $scope.allGroops;
-    //         })
-    //         .error(function(data) {
-    //             console.log('Error: ' + data);
-    //         });
+    //     request.get('http://localhost:9000/api/groups/', function (error, response, body) {
+    //         if (error) {
+    //             return console.log('Error:', error);
+    //         }
+    //         else {
+    //             $scope.allGroops = body;
+    //             console.log(body);
+    //         }
+    //     });
     // };
-
-    $scope.getAllGroops = function() {
-        request('http://localhost:9000/api/groups/', function (error, response, body) {
-            if (error) {
-                return console.log('Error:', error);
-            }
-            else {
-                $scope.allGroops = body;
-                console.log(body);
-            }
-        });
-    };
-
-    $scope.goto = function(path) {
-        $location.path(path);
-    };
 
     $scope.getGroopById = function(id) {
         $http.get('http://localhost:9000/api/groups/' + id + '/')
@@ -208,6 +208,22 @@ app.controller('searchCtrl', ['$scope', '$window', '$location', 'authInterceptor
             });
     };
 
+    // $scope.getGroopById = function() {
+    //     request.get('http://localhost:9000/api/groups/' + id + '/', function (error, response, body) {
+    //         if (error) {
+    //             return console.log('Error:', error);
+    //         }
+    //         else {
+    //             $scope.groop = body;
+    //             groopsFactory.groop = $scope.groop;
+    //         }
+    //     });
+    // };
+
+    $scope.goto = function(path) {
+        $location.path(path);
+    };
+
     $scope.getAllGroops();
     
 }]);
@@ -215,10 +231,6 @@ app.controller('searchCtrl', ['$scope', '$window', '$location', 'authInterceptor
 // Grooper Groop Controller =======================================================================================
 
 app.controller('groopCtrl', ['$scope', '$http', '$window', '$location', 'authInterceptor', 'groopsFactory', '$stateParams', function($scope, $http, $window, $location, authInterceptor, groopsFactory, $stateParams) {
-
-    // $scope.groop = groopsFactory.groops[$stateParams.id];
-    // $scope.groop = localStorage.getItem('groop');
-    // $scope.id = localStorage.getItem('id');
 
     $scope.start = function() {
         $http.get('http://localhost:9000/api/groups/' + $stateParams.id + '/')
@@ -230,13 +242,40 @@ app.controller('groopCtrl', ['$scope', '$http', '$window', '$location', 'authInt
             });
     };
 
+    // $scope.start = function() {
+    //     request.get('http://localhost:9000/api/groups/' + $stateParams.id + '/', function (error, response, body) {
+    //         if (error) {
+    //             return console.log('Error:', error);
+    //         }
+    //         else {
+    //             $scope.groop = body;
+    //         }
+    //     });
+    // };
+
     $scope.joinGroop = function(id) {
         $http.post('http://localhost:9000/api/groups/' + id + '/join-group/');
     };
+
+    // $scope.joinGroop = function(id) {
+    //     request.post('http://localhost:9000/api/groups/' + id + '/join-group/', function (error, response, body) {
+    //         if (error) {
+    //             return console.log('Error:', error);
+    //         }
+    //     });
+    // };
     
     $scope.leaveGroop = function(id) {
         $http.delete('http://localhost:9000/api/groups/' + id + '/leave-group/');
     };
+
+    // $scope.leaveGroop = function(id) {
+    //     request.delete('http://localhost:9000/api/groups/' + id + '/leave-group/', function (error, response, body) {
+    //         if (error) {
+    //             return console.log('Error:', error);
+    //         }
+    //     });
+    // };
 
     $scope.addPost = function() {
         var message = $scope.message;
@@ -244,13 +283,35 @@ app.controller('groopCtrl', ['$scope', '$http', '$window', '$location', 'authInt
         $http.post('http://localhost:9000/api/posts/', message, boardId);
     }
 
-    $scope.goto = function(path) {
-        $location.path(path);
-    };
+    // $scope.addPost = function() {
+    //     request({
+    //         url: 'http://localhost:9000/api/posts/',
+    //         method: 'POST',
+    //         body: message
+    //     }, function (error, response, body) {
+    //         if (error) {
+    //             return console.log('Error:', error);
+    //         }
+    //     });
+    // };
+
+
 
     $scope.getPosts = function() {
         $http.get('http://localhost:9000/api/posts/');
     }
+
+    // $scope.getPosts = function() {
+    //     request.get('http://localhost:9000/api/posts/', function (error, response, body) {
+    //         if (error) {
+    //             return console.log('Error:', error);
+    //         }
+    //     });
+    // };
+
+    $scope.goto = function(path) {
+        $location.path(path);
+    };
 
     $scope.start();
     
